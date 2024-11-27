827583dc4ae99f90f35c133b578329b563166399
minimanx00 <msengers98@gmail.com>
2024-11-27 14:50:14 +0100

    acckey

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8080;
+const admin = require("firebase-admin");
+const serviceAccount = require("./firebaseaccountkey.json");
+
+
+admin.initializeApp({
+
+  credential: admin.credential.cert(serviceAccount)
+
+});
+
+
+app.use(express.json());
+
+app.get('/config', (req,res) => {
+
+    res.status(200).send({
+        config:'A',
+    })
+})
+
+app.post('/stats/:id', (req,res) => {
+
+    const { id } = req.params;
+    const { logo } = req.body;
+
+
+    res.send({
+        "stats":"gotten",
+        "data":`${id} with ${logo}`
+    })
+})
 
 
 app.listen(PORT, () => console.log(`server hosting on http://localhost:${PORT}`));
