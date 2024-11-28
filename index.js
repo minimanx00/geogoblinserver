0efc4835f62f5920b9477d002dc74b8516357549
minimanx00 <msengers98@gmail.com>
2024-11-28 10:33:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-const admin = require("firebase-admin");
-const serviceAccount = require("./firebaseaccountkey.json");
 
 
-firebaseAdmin = admin.initializeApp({
-
-  credential: admin.credential.cert(serviceAccount)
-
-});
 
 
 app.use(express.json());
